760b7e02aea6e9279711c82b9204cfbfa5c64a76
Chanise Hart <niecy23@gmail.com>
2023-02-02 13:23:05 -0700

    Completed Exercise 1

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace ORM_Dapper
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            string connString = config.GetConnectionString("DefaultConnection");
+
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var repo = new DapperDepartmentRepository(conn);
+
+            var departments = repo.GetAllDepartments();
+
+            Console.WriteLine("Here is a list of all of the Departments."); 
+            foreach (var dept in departments)
+            { 
+                Console.WriteLine($"{dept.DepartmentID} {dept.Name}"); 
+            }
+
+            Console.WriteLine("Add/Type a new Department Name.");
+            var newDept = Console.ReadLine();
+            repo.InsertDepartment(newDept);
+            departments = repo.GetAllDepartments();
+
+            foreach (var dept in departments)
+            {
+                Console.WriteLine($"{dept.DepartmentID} {dept.Name}");
+            }
+
         }
     }
 }
